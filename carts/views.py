a4b3fcf6cb4ccec4d226f4ef276a684460ed8fb6
Shelton Oliveira <shelton.oliveira@gmail.com>
2023-04-27 21:09:38 -0300

    UPDATE SYSTEM TECNOLOGY

--- carts/views.py
+++ carts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 
 from accounts.forms import LoginForm, GuestForm
 from accounts.models import GuestEmail
+
 from addresses.forms import AddressForm
+from addresses.models import Address
 
 from billing.models import BillingProfile
 from orders.models import Order
@@ -15,6 +17,8 @@ def cart_home(request):
 
 def cart_update(request):
     product_id = request.POST.get('product_id')
+    if request.is_ajax():
+        print("Ajax request")
     if product_id is not None:
         try:
             product_obj = Product.objects.get(id=product_id)
@@ -42,16 +46,40 @@ def checkout_home(request):
     login_form = LoginForm()
     guest_form = GuestForm()
     address_form = AddressForm()
-
+    billing_address_id = request.session.get("billing_address_id", None)
+    shipping_address_id = request.session.get("shipping_address_id", None)
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    address_qs = None
     if billing_profile is not None:
+        if request.user.is_authenticated:
+            address_qs = Address.objects.filter(billing_profile=billing_profile)
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
-
+        if shipping_address_id:
+            order_obj.shipping_address = Address.objects.get(id = shipping_address_id)
+            del request.session["shipping_address_id"]
+        if billing_address_id:
+            order_obj.billing_address = Address.objects.get(id = billing_address_id) 
+            del request.session["billing_address_id"]
+        if billing_address_id or shipping_address_id:
+            order_obj.save()
+    if request.method == "POST":
+        #verifica se o pedido foi feito
+        is_done = order_obj.check_done()
+        if is_done:
+            order_obj.mark_paid()
+            request.session['cart_items'] = 0
+            del request.session['cart_id']
+            return redirect("cart:success")
+    
     context = {
         "object": order_obj,
         "billing_profile": billing_profile,
         "login_form": login_form,
         "guest_form": guest_form,
         "address_form": address_form,
+        "address_qs": address_qs,
     }
     return render(request, "carts/checkout.html", context)
+
+def checkout_done_view(request):
+    return render(request, "carts/checkout-done.html", {})
